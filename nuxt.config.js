76bd8ff5c1caf1b759d5a84ba2de87db389eeeaf
Gaétan Nicolas <gnicolas@appstud.com>
2018-11-15 18:15:13 +0100

    Nuxt init

---
+++ nuxt.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  /*
+  ** Build configuration
+  */
+  build: {},
+  /*
+  ** Headers
+  ** Common headers are already provided by @nuxtjs/pwa preset
+  */
+  head: {},
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#000000' },
+  /*
+  ** Customize app manifest
+  */
+  manifest: {
+    theme_color: '#000000'
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/pwa'
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <input type="file" name="pic" accept="image/*">
+      <div :class="['network',online ? 'online' : 'offline']">
+        <div class="circle"></div>
+        {{ online ? 'online' : 'offline' }}
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {Logo},
+    data () {
+      return {
+        online: true
+      }
+    },
+    mounted () {
+      this.requestFullscreen();
+      if (!window.navigator) {
+        this.online = false
+        return
+      }
+      this.online = Boolean(window.navigator.onLine)
+      window.addEventListener('offline', this._toggleNetworkStatus)
+      window.addEventListener('online', this._toggleNetworkStatus)
+    },
+    methods: {
+      _toggleNetworkStatus ({ type }) {
+        this.online = type === 'online'
+      }
+    },
+    destroyed () {
+      window.removeEventListener('offline', this._toggleNetworkStatus)
+      window.removeEventListener('online', this._toggleNetworkStatus)
+    }
+  }
+</script>
+
+<style>
+</style>
