037c166efe2149179c6b51cb3423a677a1d9a1db
Thomsath <thomas.beaupertuis@hotmail.fr>
2018-11-16 13:42:12 +0100

    Add action redirection

--- components/ShowNavbar.vue
+++ components/ShowNavbar.vue
@@ -8,7 +8,7 @@
         <label class="nav__link nav__link--label">
           <a href="add-waste">
               <img src="~assets/img/logo_rose_plein.svg">
-              <input @input="test" type="file" name="pic" class="nav__link nav__link--middle" accept="image/location.svg*">
+              <input @input="sendPicture" type="file" name="pic" class="nav__link nav__link--middle" accept="image/location.svg*">
           </a>
         </label>
         <a href="list-waste" class="nav__link nav__link--right"><img class="nav__logo" src="~assets/img/list.svg"></a>
@@ -19,8 +19,10 @@
 <script>
 export default {
   methods: {
-    test() {
+    sendPicture() {
+      const vm = this;
       console.log('test');
+      this.$router.push('add-waste');
     }
   }
 }
