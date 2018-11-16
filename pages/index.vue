4d672d05c4b29f65024235ad04a00b65e951cd0a
Gaétan Nicolas <gnicolas@appstud.com>
2018-11-16 03:13:47 +0100

    Map fullwidth

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,15 @@
 <template>
   <section class="container">
     <splash-screen v-if="splashScreenVisible"/>
-    <div>
-      <div class="navbar" v-if="navVisible">
-        <a href="new-waste" class="nav__link nav__link--left"><img class="nav__logo"src="~assets/img/location.svg"></a>
+    <div class="navbar" v-if="navVisible">
+      <a href="new-waste" class="nav__link nav__link--left"><img class="nav__logo" src="~assets/img/location.svg"></a>
 
-        <label class="nav__link nav__link--label">
-          <input type="file" name="pic" class="nav__link nav__link--middle"accept="image/location.svg*">
-        </label>
-        <a href="list-waste" class="nav__link nav__link--right"><img class="nav__logo" src="~assets/img/list.svg"></a>
-      </div>
-
-      <div :class="['network',online ? 'online' : 'offline']">
-        <div class="circle"></div>
-        {{ online ? 'online' : 'offline' }}
-      </div>
+      <label class="nav__link nav__link--label">
+        <input type="file" name="pic" class="nav__link nav__link--middle" accept="image/location.svg*">
+      </label>
+      <a href="list-waste" class="nav__link nav__link--right"><img class="nav__logo" src="~assets/img/list.svg"></a>
     </div>
-    <div style="height: 500px;width: 500px;">
-      <app-map/>
-    </div>
+    <app-map class="map"/>
   </section>
 </template>
 
@@ -36,8 +27,8 @@
     data () {
       return {
         online: true,
-        splashScreenVisible :'' ,
-        navVisible: ''
+        splashScreenVisible : false,
+        navVisible: true
       }
     },
     mounted () {
@@ -48,16 +39,20 @@
       this.online = Boolean(window.navigator.onLine)
       window.addEventListener('offline', this._toggleNetworkStatus)
       window.addEventListener('online', this._toggleNetworkStatus)
-        this.splashScreenVisible = true;
-        this.navVisible = false;
+      
+      
+      // this.splashScreenVisible = true;
+      // this.navVisible = false;
       setTimeout(
         function() {
-          const contain = document.getElementsByClassName('container');
-          const splashScreen = document.getElementsByClassName('splashScreen');
+          this.splashScreenVisible = false;
+
           
-        }, 3000
-      );
-      
+
+          // const contain = document.getElementsByClassName('container');
+          // const splashScreen = document.getElementsByClassName('splashScreen');
+          
+        }, 3000);
     },
     methods: {
       _toggleNetworkStatus ({ type }) {
@@ -67,7 +62,7 @@
     destroyed () {
       window.removeEventListener('offline', this._toggleNetworkStatus)
       window.removeEventListener('online', this._toggleNetworkStatus)
-    }
+    },
   }
 </script>
 
